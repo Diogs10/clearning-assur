4a1b57300a21d83b0024a2d7c3570daa56b7b152
diogs10 <ndiayediogal12345@gmail.com>
2025-11-20 21:03:31 +0000

    remonte role vers Keycloak

--- clearing-api/src/main/java/sn/suntelecoms/assurway/clearingapi/service/KeycloakRoleManagementService.java
+++ clearing-api/src/main/java/sn/suntelecoms/assurway/clearingapi/service/KeycloakRoleManagementService.java
@@ -12,9 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
